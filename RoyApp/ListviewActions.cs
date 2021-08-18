b6f54b6d21a7a1906b195cd0c958d0555d98c1f7
jdubar <55170139+jdubar@users.noreply.github.com>
2021-08-18 14:13:47 -0400

    Added StringBuilder intrerface

--- RoyApp/ListviewActions.cs
+++ RoyApp/ListviewActions.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RoyApp.interfaces;
+using System;
 using System.IO;
 using System.Text;
 using System.Windows.Forms;
@@ -19,7 +20,7 @@ namespace RoyApp
                 throw new ArgumentException($"'{nameof(filePath)}' cannot be null or empty.", nameof(filePath));
             }
             //make header string
-            StringBuilder result = new StringBuilder();
+            StringBuilderService result = new StringBuilderService();
             WriteCSVRow(result, listView.Columns.Count, i => includeHidden || listView.Columns[i].Width > 0, i => listView.Columns[i].Text);
 
             //export data rows
@@ -29,7 +30,7 @@ namespace RoyApp
             File.WriteAllText(filePath, result.ToString());
         }
 
-        public static void WriteCSVRow(StringBuilder result, int itemsCount, Func<int, bool> isColumnNeeded, Func<int, string> columnValue)
+        public static void WriteCSVRow(StringBuilderService result, int itemsCount, Func<int, bool> isColumnNeeded, Func<int, string> columnValue)
         {
             if (result is null)
             {
---
+++ RoyApp/interfaces/IStringBuilderService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RoyApp.interfaces
+{
+    public interface IStringBuilderService
+    {
+        IStringBuilderService Append(string line); // mimic the StringBuilder Append signature
+        StringBuilder S { get; }
+    }
+
+    public class StringBuilderService : IStringBuilderService
+    {
+        private readonly StringBuilder _actualStringBuilder;
+
+        public StringBuilder S
+        {
+            get { return _actualStringBuilder; }
+        }
+
+        public StringBuilderService() // you could also take in a parameter if you wish to initialize the variable
+        {
+            _actualStringBuilder = new StringBuilder();
+        }
+
+        public IStringBuilderService Append(string line)
+        {
+            _actualStringBuilder.Append(line);
+            return this;
+        }
+
+        public IStringBuilderService AppendLine()
+        {
+            _actualStringBuilder.AppendLine();
+            return this;
+        }
+
+        public override string ToString()
+        {
+            return _actualStringBuilder.ToString();
+        }
+    }
+}
