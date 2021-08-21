139e3bd56037fe9dcce5bab441178f4af462e1ce
jdubar <55170139+jdubar@users.noreply.github.com>
2021-08-20 22:07:53 -0400

    Updated sonar ps1 file

--- RoyApp/interfaces/IStringBuilderService.cs
+++ RoyApp/interfaces/IStringBuilderService.cs
@@ -11,29 +11,24 @@ namespace RoyApp.interfaces
     public class StringBuilderService : IStringBuilderService
     {
         private readonly StringBuilder _actualStringBuilder;
-
         public StringBuilder S
         {
             get { return _actualStringBuilder; }
         }
-
         public StringBuilderService() // you could also take in a parameter if you wish to initialize the variable
         {
             _actualStringBuilder = new StringBuilder();
         }
-
         public IStringBuilderService Append(string line)
         {
             _actualStringBuilder.Append(line);
             return this;
         }
-
         public IStringBuilderService AppendLine()
         {
             _actualStringBuilder.AppendLine();
             return this;
         }
-
         public override string ToString()
         {
             return _actualStringBuilder.ToString();
--- Sonar_KickOff.ps1
+++ Sonar_KickOff.ps1
@@ -1,33 +1,49 @@
-﻿$line = "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
-Set-Location -Path "D:\Source\Repos\RoyApp\RoyApp"
+﻿$appName = "RoyApp"
+$testName = "RoyAppTests"
+$repoPath = "D:\Source\Repos\RoyApp"
+$VSPath = "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\CommonExtensions\Microsoft\TestWindow"
+$sonarLogin = "4924be8f51f3e738c97db2c4f1531db7e938f28b"
+$sonarURL = "http://192.168.36.52:9999"
+$testFile = "$testName.dll"
+$line = "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
+#-------------------------------------------------------------------------------------------------
+Set-Location -Path "$repoPath\$appName"
+#-------------------------------------------------------------------------------------------------
 Write-Output $line
-Write-Output "Running: dotnet sonarscanner begin /k:""RoyApp"" /d:sonar.host.url=""http://192.168.36.52:9999"" /d:sonar.login=""4924be8f51f3e738c97db2c4f1531db7e938f28b"" /d:sonar.cs.vscoveragexml.reportsPaths=**/*.coveragexml"
+Write-Output "Running: dotnet sonarscanner begin /k:""$appName"" /d:sonar.host.url=""$sonarURL"" /d:sonar.login=""$sonarLogin"" /d:sonar.cs.vscoveragexml.reportsPaths=**/*.coveragexml"
 Write-Output $line
-& dotnet sonarscanner begin /k:"RoyApp" /d:sonar.host.url="http://192.168.36.52:9999" /d:sonar.login="4924be8f51f3e738c97db2c4f1531db7e938f28b" /d:sonar.cs.vscoveragexml.reportsPaths=**/*.coveragexml
+& dotnet sonarscanner begin /k:"$appName" /d:sonar.host.url="$sonarURL" /d:sonar.login="$sonarLogin" /d:sonar.cs.vscoveragexml.reportsPaths=**/*.coveragexml
+#-------------------------------------------------------------------------------------------------
 Write-Output $line
 Write-Output "Running: dotnet build"
 Write-Output $line
-dotnet build
+& dotnet build
+#-------------------------------------------------------------------------------------------------
 Write-Output $line
-Write-Output "Running: ""C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe"" /EnableCodeCoverage ""D:\Source\Repos\RoyApp\RoyAppTests\bin\Release\.netcoreapp,version=v3.1\RoyAppTests.dll"""
+Write-Output "Running: ""$VSPath\vstest.console.exe"" /EnableCodeCoverage """+$repoPath+"\$testName\bin\Release\.netcoreapp,version=v3.1\$testFile"""
 Write-Output $line
-$result = & "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe" /EnableCodeCoverage "D:\Source\Repos\RoyApp\RoyAppTests\bin\Release\.netcoreapp,version=v3.1\RoyAppTests.dll"
+$result = & "$VSPath\vstest.console.exe" /EnableCodeCoverage "$repoPath\$testName\bin\Release\.netcoreapp,version=v3.1\$testFile"
 $x = $result -like "*.coverage"
 $x = $x.Trim()
+#-------------------------------------------------------------------------------------------------
 Write-Output $line
 Write-Output "Running: dotnet test --collect ""Code Coverage"""
 Write-Output $line
 & dotnet test --collect "Code Coverage"
+#-------------------------------------------------------------------------------------------------
 Write-Output $line
-Write-Output "Running: ""D:\Source\Repos\RoyApp\RoyAppTests\bin\Release\.netcoreapp,version=v3.1\publish\CodeCoverage\amd64\CodeCoverage.exe"" analyze /output:""D:\Source\Repos\RoyApp\RoyApp\TestResults\RoyAppTests.coveragexml"" ""$x"""
+Write-Output "Running: """+$repoPath+"\$testName\bin\Release\.netcoreapp,version=v3.1\publish\CodeCoverage\amd64\CodeCoverage.exe"" analyze /output:"""+$repoPath+"\$appName\TestResults\$testName.coveragexml"" ""$x"""
 Write-Output $line
-& "D:\Source\Repos\RoyApp\RoyAppTests\bin\Release\.netcoreapp,version=v3.1\publish\CodeCoverage\amd64\CodeCoverage.exe" analyze /output:"D:\Source\Repos\RoyApp\RoyApp\TestResults\RoyAppTests.coveragexml" "$x"
+& "$repoPath\$testName\bin\Release\.netcoreapp,version=v3.1\publish\CodeCoverage\amd64\CodeCoverage.exe" analyze /output:"$repoPath\$appName\TestResults\$testName.coveragexml" "$x"
+#-------------------------------------------------------------------------------------------------
 Write-Output $line
-Write-Output "Running: ""C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe"" /Logger:trx ""D:\Source\Repos\RoyApp\RoyAppTests\bin\Release\.netcoreapp,version=v3.1\RoyAppTests.dll"""
+Write-Output "Running: ""$VSPath\vstest.console.exe"" /Logger:trx """+$repoPath+"\$testName\bin\Release\.netcoreapp,version=v3.1\$testFile"""
 Write-Output $line
-& "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe" /Logger:trx "D:\Source\Repos\RoyApp\RoyAppTests\bin\Release\.netcoreapp,version=v3.1\RoyAppTests.dll"
+& "$VSPath\vstest.console.exe" /Logger:trx "$repoPath\$testName\bin\Release\.netcoreapp,version=v3.1\$testFile"
+#-------------------------------------------------------------------------------------------------
 Write-Output $line
-Write-Output "Running: ""dotnet sonarscanner end /d:sonar.login=""4924be8f51f3e738c97db2c4f1531db7e938f28b"""
+Write-Output "Running: ""dotnet sonarscanner end /d:sonar.login=""$sonarLogin"""
 Write-Output $line
-& dotnet sonarscanner end /d:sonar.login="4924be8f51f3e738c97db2c4f1531db7e938f28b"
+& dotnet sonarscanner end /d:sonar.login="$sonarLogin"
 Write-Output $line
+#-------------------------------------------------------------------------------------------------
