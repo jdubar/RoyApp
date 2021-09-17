b891397c5779d3c96853bfa31fd0416768e33571
jdubar <55170139+jdubar@users.noreply.github.com>
2021-09-17 11:44:38 -0400

    Updated to use NUnit. TESTS NOT WORKING

--- RoyAppTests/DataServiceTests.cs
+++ RoyAppTests/DataServiceTests.cs
@@ -1,105 +1,107 @@
 ﻿using FakeItEasy;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+//using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
 using RoyApp.Services;
 
 namespace RoyApp.Tests
 {
-    [TestClass()]
     public class DataServiceTests
     {
-        private readonly IDataService _dataServiceFake;
-
-        public DataServiceTests()
+        [TestCase("12:45 PM", 12.75)]
+        public void BedtimeRaw_AMPM_Should_ConvertToDecimal(string enteredBedtimeRaw, double expectedBedtimeDec)
         {
-            _dataServiceFake = A.Fake<IDataService>();
+            // Arrange
+            var dataServiceFake = A.Fake<IDataService>();
+
+            // Act
+            A.CallTo(() => dataServiceFake.TimeToDecimal(enteredBedtimeRaw)).Returns(expectedBedtimeDec);
+            //var actual = dataServiceFake.TimeToDecimal(enteredBedtimeRaw);
+
+            // Assert
+            Assert.That(dataServiceFake.TimeToDecimal(enteredBedtimeRaw), Is.EqualTo(expectedBedtimeDec));
         }
+        //[TestMethod()]
+        //public void BedtimeRaw_AMPM_ShouldNotEqual_ExpectedDecimal()
+        //{
+        //    var mock = A.Fake<IDataService>();
 
-        [TestMethod()]
-        public void BedtimeRaw_AMPM_Should_ConvertToDecimal()
-        {
-            string enteredBedtimeRaw = "12:45 PM";
-            double expectedBedtimeDec = 12.75;
+        //    string enteredBedtimeRaw = "1:45 AM";
+        //    double expectedBedtimeDec = 12.75;
 
-            double actual = _dataServiceFake.TimeToDecimal(enteredBedtimeRaw);
-            Assert.AreEqual(expectedBedtimeDec, actual);
-        }
-        [TestMethod()]
-        public void BedtimeRaw_AMPM_ShouldNotEqual_ExpectedDecimal()
-        {
-            string enteredBedtimeRaw = "1:45 AM";
-            double expectedBedtimeDec = 12.75;
+        //    A.CallTo(() => mock.TimeToDecimal(enteredBedtimeRaw)).Returns(expectedBedtimeDec);
 
-            double actual = _dataServiceFake.TimeToDecimal(enteredBedtimeRaw);
-            Assert.AreNotEqual(expectedBedtimeDec, actual);
-        }
-        [TestMethod()]
-        public void BedtimeRaw_GreaterThanTwelve_ShouldEqual_ExpectedDecimal()
-        {
-            string enteredBedtimeRaw = "1345";
-            double expectedBedtimeDec = 13.75;
+        //    double actual = _dataServiceFake.TimeToDecimal(enteredBedtimeRaw);
+        //    A.Equals
+        //    Assert.AreNotEqual(expectedBedtimeDec, actual);
+        //}
+        //[TestMethod()]
+        //public void BedtimeRaw_GreaterThanTwelve_ShouldEqual_ExpectedDecimal()
+        //{
+        //    string enteredBedtimeRaw = "1345";
+        //    double expectedBedtimeDec = 13.75;
 
-            double actual = _dataServiceFake.TimeToDecimal(enteredBedtimeRaw);
-            Assert.AreEqual(expectedBedtimeDec, actual);
-        }
-        [TestMethod()]
-        public void BedtimeRaw_AMPM_LessThanTwelve_ShouldEqual_ExpectedDecimal()
-        {
-            string enteredBedtimeRaw = "145PM";
-            double expectedBedtimeDec = 13.75;
+        //    double actual = _dataServiceFake.TimeToDecimal(enteredBedtimeRaw);
+        //    Assert.AreEqual(expectedBedtimeDec, actual);
+        //}
+        //[TestMethod()]
+        //public void BedtimeRaw_AMPM_LessThanTwelve_ShouldEqual_ExpectedDecimal()
+        //{
+        //    string enteredBedtimeRaw = "145PM";
+        //    double expectedBedtimeDec = 13.75;
 
-            double actual = _dataServiceFake.TimeToDecimal(enteredBedtimeRaw);
-            Assert.AreEqual(expectedBedtimeDec, actual);
-        }
-        [TestMethod()]
-        public void BedtimeRaw_LessThanThreeChars_ShouldReturnZero()
-        {
-            string enteredBedtimeRaw = "1";
-            double expectedResult = 0;
+        //    double actual = _dataServiceFake.TimeToDecimal(enteredBedtimeRaw);
+        //    Assert.AreEqual(expectedBedtimeDec, actual);
+        //}
+        //[TestMethod()]
+        //public void BedtimeRaw_LessThanThreeChars_ShouldReturnZero()
+        //{
+        //    string enteredBedtimeRaw = "1";
+        //    double expectedResult = 0;
 
-            double actual = _dataServiceFake.TimeToDecimal(enteredBedtimeRaw);
-            Assert.AreEqual(expectedResult, actual);
-        }
+        //    double actual = _dataServiceFake.TimeToDecimal(enteredBedtimeRaw);
+        //    Assert.AreEqual(expectedResult, actual);
+        //}
 
-        [TestMethod()]
-        public void TimeDuration_ShouldEqual_ExpectedDuration()
-        {
-            string enteredBedtimeRec = "22";
-            string enteredWaketimeRec = "8";
-            double expectedDuration = 10;
+        //[TestMethod()]
+        //public void TimeDuration_ShouldEqual_ExpectedDuration()
+        //{
+        //    string enteredBedtimeRec = "22";
+        //    string enteredWaketimeRec = "8";
+        //    double expectedDuration = 10;
 
-            double actual = _dataServiceFake.TimeDuration(enteredBedtimeRec, enteredWaketimeRec);
-            Assert.AreEqual(expectedDuration, actual);
-        }
-        [TestMethod()]
-        public void TimeDuration_ShouldNotEqual_ExpectedDuration()
-        {
-            string enteredBedtimeRec = "12.75";
-            string enteredWaketimeRec = "13.75";
-            double expectedDuration = 2;
+        //    double actual = _dataServiceFake.TimeDuration(enteredBedtimeRec, enteredWaketimeRec);
+        //    Assert.AreEqual(expectedDuration, actual);
+        //}
+        //[TestMethod()]
+        //public void TimeDuration_ShouldNotEqual_ExpectedDuration()
+        //{
+        //    string enteredBedtimeRec = "12.75";
+        //    string enteredWaketimeRec = "13.75";
+        //    double expectedDuration = 2;
 
-            double actual = _dataServiceFake.TimeDuration(enteredBedtimeRec, enteredWaketimeRec);
-            Assert.AreNotEqual(expectedDuration, actual);
-        }
+        //    double actual = _dataServiceFake.TimeDuration(enteredBedtimeRec, enteredWaketimeRec);
+        //    Assert.AreNotEqual(expectedDuration, actual);
+        //}
 
-        [TestMethod()]
-        public void TimeAverage_ShouldEqual_ExpectedAverage()
-        {
-            double bedtimeTotal = 50;
-            int bedtimeCount = 5;
-            double expectedAverage = 10;
+        //[TestMethod()]
+        //public void TimeAverage_ShouldEqual_ExpectedAverage()
+        //{
+        //    double bedtimeTotal = 50;
+        //    int bedtimeCount = 5;
+        //    double expectedAverage = 10;
 
-            double actual = _dataServiceFake.TimeAverage(bedtimeTotal, bedtimeCount);
-            Assert.AreEqual(expectedAverage, actual);
-        }
-        [TestMethod()]
-        public void TimeAverage_ShouldNotEqual_ExpectedAverage()
-        {
-            double bedtimeTotal = 50;
-            int bedtimeCount = 5;
-            double expectedAverage = 5;
+        //    double actual = _dataServiceFake.TimeAverage(bedtimeTotal, bedtimeCount);
+        //    Assert.AreEqual(expectedAverage, actual);
+        //}
+        //[TestMethod()]
+        //public void TimeAverage_ShouldNotEqual_ExpectedAverage()
+        //{
+        //    double bedtimeTotal = 50;
+        //    int bedtimeCount = 5;
+        //    double expectedAverage = 5;
 
-            double actual = _dataServiceFake.TimeAverage(bedtimeTotal, bedtimeCount);
-            Assert.AreNotEqual(expectedAverage, actual);
-        }
+        //    double actual = _dataServiceFake.TimeAverage(bedtimeTotal, bedtimeCount);
+        //    Assert.AreNotEqual(expectedAverage, actual);
+        //}
     }
 }
--- RoyAppTests/ListviewActionsTests.cs
+++ RoyAppTests/ListviewActionsTests.cs
@@ -8,25 +8,25 @@ namespace RoyApp.Tests
     [TestClass()]
     public class ListviewActionsTests
     {
-        private readonly IFileService _fileService;
+        //private readonly IFileService _fileService;
 
-        [TestMethod()]
-        public void ListViewToCSV_ListViewIsNull_ShouldThrowException()
-        {
-            List<List<string>> TestList = null;
-            string[] headers = { "id", "data1", "data2" };
-            var expectedParamName = "listView";
-            string FilePath = @"C:\test.csv";
+        //[TestMethod()]
+        //public void ListViewToCSV_ListViewIsNull_ShouldThrowException()
+        //{
+        //    List<List<string>> TestList = null;
+        //    string[] headers = { "id", "data1", "data2" };
+        //    var expectedParamName = "listView";
+        //    string FilePath = @"C:\test.csv";
 
-            try
-            {
-                _fileService.WriteToCsv(TestList, headers, FilePath);
-            }
-            catch (ArgumentException ex)
-            {
-                Assert.AreEqual(expectedParamName, ex.ParamName);
-            }
-        }
+        //    try
+        //    {
+        //        _fileService.WriteToCsv(TestList, headers, FilePath);
+        //    }
+        //    catch (ArgumentException ex)
+        //    {
+        //        Assert.AreEqual(expectedParamName, ex.ParamName);
+        //    }
+        //}
         //[TestMethod()]
         //public void ListViewToCSV_FilePathIsNull_ShouldThrowException()
         //{
